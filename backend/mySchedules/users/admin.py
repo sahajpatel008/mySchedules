2dc3b625d001c8677e72329034683d9867f64e9c
sahajD360 <sahajpatel008@gmail.com>
2024-11-28 18:02:13 -0800

    manager and user different types added. Admin panel added

--- backend/mySchedules/users/admin.py
+++ backend/mySchedules/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    # Display 'username', 'email', and 'role' in the list view
+    list_display = ('username', 'email', 'role', 'is_active', 'is_staff')
+
+    # Modify the 'fieldsets' to include custom fields without duplicating
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role',)}),  # Only add 'role' here, since 'email' is already part of the default fieldsets
+    )
+
+    # Modify the 'add_fieldsets' for the user creation form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'role'),
+        }),
+    ) + UserAdmin.add_fieldsets  # Include the default add fieldsets at the end
+
+# Register the custom UserAdmin
+admin.site.register(User, CustomUserAdmin)
--- backend/mySchedules/users/models.py
+++ backend/mySchedules/users/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('manager', "Manager"),
+        ('employee', "Employee")
+    )
     username = models.CharField(max_length=100, primary_key=True, unique=True)
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
